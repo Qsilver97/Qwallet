1e57f9efb81fdaa0ad39822798bb9d2282564493
konan <ikedahiroshi517@gmail.com>
2024-03-21 14:24:17 +0000

    Create dashboard

--- app/client/src/pages/Dashboard.tsx
+++ app/client/src/pages/Dashboard.tsx
@@ -1,12 +1,53 @@
+import { faGear } from "@fortawesome/free-solid-svg-icons";
+import { faArrowDown } from "@fortawesome/free-solid-svg-icons/faArrowDown";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 
 const Dashboard: React.FC = () => {
     return (
-        <>
-            <div className="bg-light dark:bg-dark text-light dark:text-dark">
-                This text and background color will change based on the theme.
+        <div className="w-[calc(100%-20px)] max-w-[1368px] bg-[rgba(3,35,61,0.8)] h-[calc(100vh-100px)] m-[0_10px] overflow-y-auto z-0 rounded-[10px] shadow-[0_15px_25px_rgba(0,0,0,0.5)] text-white mx-auto">
+            <header className="p-[20px_60px] flex justify-between items-center border-b border-white text-[16px]">
+                <div className="cursor-pointer">
+                    <img src="images/logo.png" className="w-[50px]" />
+                </div>
+                <div className="cursor-pointer">
+                    <span className="p-[5px] shadow-[0_2px_3px_rgba(0,0,0,0.5)] rounded-[5px]">
+                        MVPYRACGNJBMJGDLLAXUDXHXFOXBOBWCBZQBAXSUTGJXOBRLZVCTBDPCXPMK
+                        <FontAwesomeIcon icon={faArrowDown} />
+                    </span>
+                </div>
+                <div className="flex items-center gap-[10px] cursor-pointer">
+                    <FontAwesomeIcon className="text-[32px]" icon={faGear} />
+                </div>
+            </header>
+            <div className="p-[20px_60px] ">
+                <div>
+                    <h3 className="text-[1.75rem]">Balance: 0</h3>
+                    <h3 className="text-[1.75rem]">Tick: 130928</h3>
+                </div>
+                <div className="mt-[20px]">
+                    <div className="">
+                        <input className="text-white p-[10px] mr-[5px] border-[1.5px] border-[#17517a] rounded-[5px] w-[720px] outline-none bg-transparent" />
+                        <input className="text-white p-[10px] mr-[5px] border-[1.5px] border-[#17517a] rounded-[5px] w-[120px] outline-none bg-transparent " />
+                        <button className="outline-none p-[10px_20px] bg-[#17517a] border-none rounded-[5px] text-white text-[16px] cursor-pointer transition-bg duration-300 ease">Send</button>
+                    </div>
+                    <div className="mt-[40px] max-h-[1000px] overflow-auto">
+                        <h3 className="text-[1.75rem]">Activity</h3>
+                        <table className="text-[#bbb] w-full max-w-[100%] mb-[1rem] bg-transparent text-left">
+                            <thead>
+                                <tr>
+                                    <th className="border-b border-[#dee2e6] border-t-0 p-[0.75rem]">Date</th>
+                                    <th className="border-b border-[#dee2e6] border-t-0 p-[0.75rem]">Type</th>
+                                    <th className="border-b border-[#dee2e6] border-t-0 p-[0.75rem]">Detail</th>
+                                    <th className="border-b border-[#dee2e6] border-t-0 p-[0.75rem]">Amount</th>
+                                </tr>
+                            </thead>
+                            <tbody></tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
-        </>
+        </div>
     )
 }
 
