2494360c7f1a7d40bda5763e36772d102c2be20f
konan <ikedahiroshi517@gmail.com>
2024-03-22 11:30:04 +0000

    Test pkg

--- app/client/src/pages/Dashboard.tsx
+++ app/client/src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { faCopy, faGear, faPlus, faTimes, faTrash } from "@fortawesome/free-solid-svg-icons";
+import { faCopy, faPlus, faTimes, faTrash } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useEffect, useState } from "react";
 import Modal from "../components/common/Modal";
