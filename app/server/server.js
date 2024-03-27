a2a91445e213eea43886f838e4c7adad8f76f39c
Konan <ikedahiroshi517@gmail.com>
2024-03-27 01:21:23 -0700

    Seperate electron and dev

--- app/server/server.js
+++ app/server/server.js
@@ -1,64 +1,6 @@
 const { app: electronApp, BrowserWindow } = require('electron');
-const fs = require('fs').promises;
 const path = require('path');
-const express = require('express');
-const app = express();
-const http = require('http').createServer(app);
-const cors = require('cors');
-
-// Import custom modules and configurations
-const router = require('./routes.js');
-const socketManager = require('./managers/socketManager')
-const wasmManager = require('./managers/wasmManager')
-const stateManager = require('./managers/stateManager')
-const { PORT, FRONTEND_URL } = require('./utils/constants');
-
-async function createDirectoryIfNotExists(directoryPath) {
-    try {
-        await fs.mkdir(directoryPath, { recursive: true });
-    } catch (error) {
-        console.error(`Error creating directory ${directoryPath}:`, error);
-    }
-}
-
-const keyDirectoryPath = path.join(__dirname, 'keys');
-createDirectoryIfNotExists(keyDirectoryPath);
-
-async function init() {
-    // Initialize WebSocket communication, WASM manager and UserState manager
-    const io = await socketManager.init(http);
-    // const liveSocket = await socketManager.initLiveSocket();
-    wasmManager.init();
-    stateManager.init();
-
-    // Import and use socket controller with initialized WebSocket (io)
-    require('./controllers/socketController')(io)
-    // require('./controllers/liveSocketController')(liveSocket)
-}
-
-init()
-
-// Middleware to enable CORS with dynamic origin support
-app.use(cors({ origin: FRONTEND_URL }))
-
-// Serve static files from the 'dist' directory
-app.use(express.static(path.join(__dirname, '/dist')));
-
-// Use built-in middleware for parsing JSON and URL-encoded bodies
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Define routes
-app.use('/api', router)
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
-});
-
-// Start the HTTP server listening on the specified port
-http.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server is running on port ${PORT}`);
-});
-
+const { startServer } = require('./main');
 function createWindow() {
     // Create the browser window.
     const mainWindow = new BrowserWindow({
@@ -79,7 +21,8 @@ function createWindow() {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-electronApp.whenReady().then(() => {
+electronApp.whenReady().then(async () => {
+    await startServer()
     createWindow();
 
     electronApp.on('activate', function () {
