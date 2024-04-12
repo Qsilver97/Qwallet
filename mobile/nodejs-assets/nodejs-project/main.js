5c5cd095e1937c249c7c55086f43507bcf122bb3
RealWorld777 <discord.dream120@gmail.com>
2024-04-12 16:26:50 -0700

    fix: Delete Permission Directory Sending from Frontend

--- mobile/nodejs-assets/nodejs-project/main.js
+++ mobile/nodejs-assets/nodejs-project/main.js
@@ -8,34 +8,25 @@ const fs = require("fs").promises;
 const createDirectoryIfNotExists = async (directoryPath) => {
   try {
     await fs.mkdir(directoryPath, { recursive: true });
+    console.log(directoryPath)
   } catch (error) {
     console.log(`Error creating directory ${directoryPath}:${error}`);
   }
 };
 
-wasmManager.init();
-stateManager.init();
-// const init = async (dir) => {
-//   const result = await wasmManager.ccall({command:`keysdir ${dir}`, flag: "keysdir"})
-//   console.log(dir)
-//   console.log("Create_Directory_RESULT\n", result)
-//   const keyDirectoryPath = path.join(dir, "/qwallet/keys");
-//   await createDirectoryIfNotExists(keyDirectoryPath);
-// };
+const init = async () => {
+  const keyDirectoryPath = path.join(__dirname, "keys");
+  await createDirectoryIfNotExists(keyDirectoryPath);
+  // const result = await wasmManager.ccall({command:`keysdir ${path.join(__dirname, 'keys')}`, flag: "keysdir"})
+  wasmManager.init();
+  stateManager.init();
+};
 
-// init();
+init();
 
 rn_bridge.channel.on("message", async (msg) => {
   try {
     const message = JSON.parse(msg);
-    if(message.action === "SET_FILES_DIR"){
-      const dir = message.data?.path;
-      console.log(dir)
-      const result = await wasmManager.ccall({command:`keysdir ${dir}`, flag: "keysdir"})
-      console.log("Create_Directory_RESULT\n", result)
-      const keyDirectoryPath = path.join(dir, "/qwallet/keys");
-      await createDirectoryIfNotExists(keyDirectoryPath);
-    }
 
     if (message.action === "C2S/login") {
       const success = await login({ password: message.data?.password });
