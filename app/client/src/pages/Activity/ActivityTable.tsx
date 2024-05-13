9dab5b0d1cec43fa7fb00b23f7a9d04a463075e5
Konan <ikedahiroshi517@gmail.com>
2024-05-13 17:01:02 +0900

    activity table

--- app/client/src/pages/Activity/ActivityTable.tsx
+++ app/client/src/pages/Activity/ActivityTable.tsx
@@ -6,100 +6,66 @@ import { useAuth } from "../../contexts/AuthContext";
 const ActivityTable = () => {
     const { currentAddress } = useAuth();
     const [loading, setLoading] = useState<boolean>(true);
-    const [history, setHistory] = useState<any>();
+    const [history, setHistory] = useState<[string, string, string, string, string, number][]>([]);
     useEffect(() => {
         async function init() {
             try {
                 setLoading(true);
                 const resp = await axios.post(`${SERVER_URL}/api/history`, { address: currentAddress });
                 console.log(resp.data);
-                setHistory(resp.data);
+                setHistory(resp.data.history);
             } catch (error) {
 
+            } finally {
+                setLoading(false);
             }
         }
         init();
-    }, []);
-    const data = [
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-        {
-            product: 'NTMG........................JLKS',
-            txID: 1234567,
-            address: 'NTMG........................JLKS',
-            amount: 250
-        },
-    ]
+    }, [currentAddress]);
 
     return (
         <div className="flex flex-col gap-8">
-            <div className="grid grid-cols-[1fr_0.6fr_1fr_0.6fr] gap-x-4">
-                <div className="font-medium font-Inter text-sm text-left text-dark-gray">TXID</div>
-                <div className="font-medium font-Inter text-sm text-left text-dark-gray">TXID</div>
-                <div className="font-medium font-Inter text-sm text-left text-dark-gray">ADDRESS</div>
-                <div className="font-medium font-Inter text-sm text-left text-dark-gray">AMOUNT</div>
-            </div>
-            <div className="flex flex-col gap-6">
-                {data.map(({ product, txID, address, amount }, index) => (
-                    <div key={index} className="grid grid-cols-[min-content_0.6fr_1fr_0.6fr] gap-x-5">
-                        <div className="font-semibold font-Inter">{product}</div>
-                        <div className="font-semibold font-Inter">{txID}</div>
-                        <div className="font-semibold font-Inter">{address}</div>
-                        <div className="font-semibold font-Inter">{amount}</div>
+            <div className="flex flex-col">
+                <div className="-m-1.5 overflow-x-auto">
+                    <div className="p-1.5 min-w-full inline-block align-middle">
+                        <div className="overflow-hidden">
+                            {loading ?
+                                <div className={`flex items-center justify-center min-h-screen`}>
+                                    <div className="flex flex-col justify-center items-center">
+                                        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-white"></div>
+                                        <p className="mt-3 text-lg">Loading...</p>
+                                    </div>
+                                </div> :
+                                <table className="min-w-full divide-y divide-gray-200 dark:divide-neutral-700">
+                                    <thead>
+                                        <tr>
+                                            <th scope="col" className="px-1 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">TXID</th>
+                                            <th scope="col" className="px-1 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">TICK</th>
+                                            <th scope="col" className="px-1 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">ADDRESS</th>
+                                            <th scope="col" className="px-1 py-3 text-start text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">AMOUNT</th>
+                                            {/* <th scope="col" className="px-1 py-3 text-end text-xs font-medium text-gray-500 uppercase dark:text-neutral-500">Action</th> */}
+                                        </tr>
+                                    </thead>
+                                    <tbody className="divide-y divide-gray-200 dark:divide-neutral-700 font-mono">
+                                        {
+                                            history.map((item, idx) => {
+                                                return <tr key={idx}>
+                                                    <td className="px-1 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">{item[1].slice(0, 10)}...</td>
+                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200 font-mono">{item[0]}</td>
+                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{item[2].slice(0, 10)}...</td>
+                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{item[3]}</td>
+                                                    {/* <td className="px-1 py-4 whitespace-nowrap text-end text-sm font-medium">
+                                                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-400 hover:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:hover:text-blue-400">Select</button>
+                                                    </td> */}
+                                                </tr>
+                                            })
+                                        }
+                                    </tbody>
+                                </table>
+                            }
+                        </div>
                     </div>
-                ))}
+                </div>
             </div>
         </div>
     );
