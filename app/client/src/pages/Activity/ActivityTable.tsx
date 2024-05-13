ebd75f110e3a3cc5a031f99cf8ae2a6df100d796
Konan <ikedahiroshi517@gmail.com>
2024-05-13 16:27:08 +0900

    Implement activity

--- app/client/src/pages/Activity/ActivityTable.tsx
+++ app/client/src/pages/Activity/ActivityTable.tsx
@@ -1,4 +1,25 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import { SERVER_URL } from "../../utils/constants";
+import { useAuth } from "../../contexts/AuthContext";
+
 const ActivityTable = () => {
+    const { currentAddress } = useAuth();
+    const [loading, setLoading] = useState<boolean>(true);
+    const [history, setHistory] = useState<any>();
+    useEffect(() => {
+        async function init() {
+            try {
+                setLoading(true);
+                const resp = await axios.post(`${SERVER_URL}/api/history`, { address: currentAddress });
+                console.log(resp.data);
+                setHistory(resp.data);
+            } catch (error) {
+
+            }
+        }
+        init();
+    }, []);
     const data = [
         {
             product: 'NTMG........................JLKS',
@@ -70,7 +91,7 @@ const ActivityTable = () => {
                 <div className="font-medium font-Inter text-sm text-left text-dark-gray">ADDRESS</div>
                 <div className="font-medium font-Inter text-sm text-left text-dark-gray">AMOUNT</div>
             </div>
-            <div className="flex flex-col gap-6">     
+            <div className="flex flex-col gap-6">
                 {data.map(({ product, txID, address, amount }, index) => (
                     <div key={index} className="grid grid-cols-[min-content_0.6fr_1fr_0.6fr] gap-x-5">
                         <div className="font-semibold font-Inter">{product}</div>
@@ -82,6 +103,6 @@ const ActivityTable = () => {
             </div>
         </div>
     );
-  };
-  
-  export default ActivityTable
+};
+
+export default ActivityTable
