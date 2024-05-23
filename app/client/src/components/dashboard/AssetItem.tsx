6b0d8b80ce3196a1b2a0d756dd62ac96f478dc62
Denis <dngaspar5@gmail.com>
2024-05-23 17:43:44 +0200

    fix: dashboard percentage

--- app/client/src/components/dashboard/AssetItem.tsx
+++ app/client/src/components/dashboard/AssetItem.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { assetsItems } from '../../utils/constants';
 import { useAuth } from '../../contexts/AuthContext';
 
-const AssetItem: React.FC<{ token: string }> = ({ token }) => {
+const AssetItem: React.FC<{ token: string, percent: number }> = ({ token, percent }) => {
     const { tokenBalances, currentAddress } = useAuth();
-    const item = assetsItems.find((k) => k.name == token) || assetsItems[0]
+    const item = assetsItems.find((k) => k.name == token) || assetsItems[0];
     return (
         <div className="flex flex-col items-center justify-between pb-3 text-white">
             <div className="flex items-center justify-between w-full">
@@ -24,11 +24,11 @@ const AssetItem: React.FC<{ token: string }> = ({ token }) => {
                 <div className="w-full bg-[#353535] rounded-full h-2">
                     <div
                         className={`${item.colorClassName} h-2 rounded-full`}
-                        style={{ width: `${item.percentage}%` }}
+                        style={{ width: `${percent}%` }}
                     ></div>
                 </div>
                 <div className="ml-4">
-                    <span className="text-sm font-semibold">{item.percentage.toFixed(2)}%</span>
+                    <span className="text-sm font-semibold">{percent.toFixed(2)}%</span>
                 </div>
             </div>
         </div>
