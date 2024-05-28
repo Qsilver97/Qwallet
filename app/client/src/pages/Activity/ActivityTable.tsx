1aa1562e0b0418bbcabc23fe75ea99792d3d76bd
Denis <dngaspar5@gmail.com>
2024-05-28 11:12:07 +0900

    add copy button and update sctx field

--- app/client/src/pages/Activity/ActivityTable.tsx
+++ app/client/src/pages/Activity/ActivityTable.tsx
@@ -1,8 +1,11 @@
 import axios from "axios";
-import { formatNumberWithCommas } from "../../utils/helper";
+import { formatNumberWithCommas, handleCopy } from "../../utils/helper";
 import { HistoryEntry } from "./Activity";
 import { SERVER_URL } from "../../utils/constants";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import _ from 'lodash';
+import { ClipboardDocumentIcon } from '@heroicons/react/24/solid';
+
 
 interface ActivityTableProps {
     history: HistoryEntry[];
@@ -12,13 +15,12 @@ interface ActivityTableProps {
 
 const ActivityTable = ({ history, loading }: ActivityTableProps) => {
     const [hoverIdx, setHoverIdx] = useState<number>();
-    const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
     const [hoverItem, setHoverItem] = useState<string>();
     const [itemLoading, setItemLoading] = useState<boolean>(false);
 
-    const handleMouseEnter = async (event: React.MouseEvent<HTMLDivElement, MouseEvent>, item: HistoryEntry, idx: number) => {
+
+    const handleMouseEnter = useCallback(_.throttle(async (_: React.MouseEvent<HTMLSpanElement, MouseEvent>, item: HistoryEntry, idx: number) => {
         setItemLoading(true);
-        setMousePosition({ x: event.clientX, y: event.clientY });
         setHoverIdx(idx);
         const command = `${item[1]} ${item[0]}`;
         try {
@@ -30,13 +32,8 @@ const ActivityTable = ({ history, loading }: ActivityTableProps) => {
         } finally {
             setItemLoading(false);
         }
-    };
+    }, 100), []);
 
-    const handleMouseMove = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        if (hoverIdx != undefined) {
-            setMousePosition({ x: event.clientX, y: event.clientY });
-        }
-    };
 
     const handleMouseLeave = () => {
         setHoverIdx(undefined);
@@ -71,30 +68,61 @@ const ActivityTable = ({ history, loading }: ActivityTableProps) => {
                                             history.map((item, idx) => {
                                                 return <tr
                                                     key={idx}
-                                                    // onMouseOver={() => handleFetchTxInfo(item, idx)}
-                                                    // onMouseDown={handleMouseDown}
                                                     onMouseEnter={(e) => { handleMouseEnter(e, item, idx) }}
-                                                    onMouseMove={handleMouseMove}
                                                     onMouseLeave={handleMouseLeave}
-                                                // className="relative"
+                                                    className="relative px-1 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200"
                                                 >
-                                                    <td className="px-1 py-4 whitespace-nowrap text-sm font-medium text-gray-800 dark:text-neutral-200">{item[1]}</td>
-                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200 font-mono">{formatNumberWithCommas(parseInt(item[0]))}</td>
-                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{item[2]}</td>
-                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">{formatNumberWithCommas(parseInt(item[3]))}</td>
-                                                    {/* <td className="px-1 py-4 whitespace-nowrap text-end text-sm font-medium">
-                                                        <button type="button" className="inline-flex items-center gap-x-2 text-sm font-semibold rounded-lg border border-transparent text-blue-400 hover:text-blue-600 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-400 dark:hover:text-blue-400">Select</button>
-                                                    </td> */}
+                                                    <td
+                                                        className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200 font-mono"
+                                                    >
+                                                        <div className="flex gap-1">
+                                                            <span>
+                                                                {item[1]}
+                                                            </span>
+                                                            <span>
+                                                                <ClipboardDocumentIcon className="h-5 w-5 cursor-pointer text-white hover:text-red-400" onClick={() => handleCopy(item[1])} />
+                                                            </span>
+                                                        </div>
+                                                    </td>
+                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200 font-mono">
+                                                        <div className="flex gap-1">
+                                                            <span>
+                                                                {formatNumberWithCommas(parseInt(item[0]))}
+                                                            </span>
+                                                            <span>
+                                                                <ClipboardDocumentIcon className="h-5 w-5 cursor-pointer text-white hover:text-red-400" onClick={() => handleCopy(item[0])} />
+                                                            </span>
+                                                        </div>
+                                                    </td>
+                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                                        <div className="flex gap-1">
+                                                            <span>
+                                                                {item[2]}
+                                                            </span>
+                                                            <span>
+                                                                <ClipboardDocumentIcon className="h-5 w-5 cursor-pointer text-white hover:text-red-400" onClick={() => handleCopy(item[2])} />
+                                                            </span>
+                                                        </div>
+                                                    </td>
+                                                    <td className="px-1 py-4 whitespace-nowrap text-sm text-gray-800 dark:text-neutral-200">
+                                                        <div className="flex gap-1">
+                                                            <span>
+                                                                {formatNumberWithCommas(parseInt(item[3]))}
+                                                            </span>
+                                                            <span>
+                                                                <ClipboardDocumentIcon className="h-5 w-5 cursor-pointer text-white hover:text-red-400" onClick={() => handleCopy(item[3])} />
+                                                            </span>
+                                                        </div>
+                                                    </td>
                                                     {hoverIdx != undefined && hoverIdx == idx && item[2].startsWith('BAAAAAAA') &&
-                                                        (itemLoading ?
-                                                            <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-white"></div> :
-                                                            <div
-                                                                className="fixed bg-white text-black border border-gray-300 shadow-lg p-2 break-all break-words max-w-[500px]"
-                                                                style={{ top: mousePosition.y, left: mousePosition.x }}
-                                                            >
-                                                                {hoverItem}
-                                                            </div>
-                                                        )
+                                                        <div
+                                                            className="absolute bg-white text-black border border-gray-300 shadow-lg p-2 max-w-[300px] top-0 left-0 break-words overflow-hidden whitespace-normal z-10"
+                                                        >
+                                                            {itemLoading ?
+                                                                <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-black"></div> :
+                                                                hoverItem
+                                                            }
+                                                        </div>
                                                     }
                                                 </tr>
                                             })
